59843ae291666579d1cc812fdac25cc5f3f55577
Marvin Friedrich <contact@marvinf.com>
2026-09-15 23:36:39 +0200

    options/linux: Implement memalign

--- options/linux/generic/malloc.cpp
+++ options/linux/generic/malloc.cpp
@@ -1,7 +1,20 @@
 #include <bits/ensure.h>
+#include <errno.h>
+#include <frg/bitops.hpp>
 #include <malloc.h>
+#include <stdlib.h>
 
-void *memalign(size_t, size_t) {
-	__ensure(!"Not implemented");
-	__builtin_unreachable();
+void *memalign(size_t alignment, size_t size) {
+	// Unlike posix_memalign(), memalign() permits alignments smaller than sizeof(void *).
+	if (alignment < sizeof(void *))
+		alignment = sizeof(void *);
+	alignment = size_t{1} << frg::ceil_log2(alignment);
+
+	void *ptr = nullptr;
+	int ret = posix_memalign(&ptr, alignment, size);
+	if (ret) {
+		errno = ret;
+		return nullptr;
+	}
+	return ptr;
 }
